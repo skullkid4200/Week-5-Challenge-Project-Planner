f25cf59da7149ac083f9b1636e4e338b625ceaf3
skullkid4200 <jackeinhorn123@gmail.com>
2023-01-04 16:37:31 -0500

    got all the local storage for the daily tasks finished. big win! just have to do color-coding now

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,39 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var saveButton = document.querySelector("button");
-var inputTask = document.querySelector("textarea")
+var saveButton9 = document.querySelector("#button9");
+var saveButton10 = document.querySelector("#button10");
+var saveButton11 = document.querySelector("#button11");
+var saveButton12 = document.querySelector("#button12");
+var saveButton1 = document.querySelector("#button1");
+var saveButton2 = document.querySelector("#button2");
+var saveButton3 = document.querySelector("#button3");
+var saveButton4 = document.querySelector("#button4");
+var saveButton5 = document.querySelector("#button5");
+var inputTask = document.querySelector("#textarea9")
+var inputTask2 = document.querySelector("#textarea10")
+var inputTask3 = document.querySelector("#textarea11")
+var inputTask4 = document.querySelector("#textarea12")
+var inputTask5 = document.querySelector("#textarea1")
+var inputTask6 = document.querySelector("#textarea2")
+var inputTask7 = document.querySelector("#textarea3")
+var inputTask8 = document.querySelector("#textarea4")
+var inputTask9 = document.querySelector("#textarea5")
 
 
+document.getElementById('textarea9').value = localStorage.getItem("tasks9");
+document.getElementById('textarea10').value = localStorage.getItem("tasks10");
+document.getElementById('textarea11').value = localStorage.getItem("tasks11");
+document.getElementById('textarea12').value = localStorage.getItem("tasks12");
+document.getElementById('textarea1').value = localStorage.getItem("tasks1");
+document.getElementById('textarea2').value = localStorage.getItem("tasks2");
+document.getElementById('textarea3').value = localStorage.getItem("tasks3");
+document.getElementById('textarea4').value = localStorage.getItem("tasks4");
+document.getElementById('textarea5').value = localStorage.getItem("tasks5");
 
 var today = dayjs();
 $('#currentDay').text(today.format('[Today is] dddd, MMM D, YYYY'));
 
-
-localStorage.getItem("tasks", inputTask.value);
 
 
 $(function () {
@@ -21,12 +44,42 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  saveButton.addEventListener("click", function () {
-
-    // var inputTask = document.getElementsByClassName("col-8 col-md-10 description");
-    localStorage.setItem("tasks", inputTask.value);
-
+  saveButton9.addEventListener("click", function () {
+    localStorage.setItem("tasks9", inputTask.value);
   });
+
+  saveButton10.addEventListener("click", function () {
+    localStorage.setItem("tasks10", inputTask2.value);
+  });
+
+  saveButton11.addEventListener("click", function () {
+    localStorage.setItem("tasks11", inputTask3.value);
+  });
+
+  saveButton12.addEventListener("click", function () {
+    localStorage.setItem("tasks12", inputTask4.value);
+  });
+
+  saveButton1.addEventListener("click", function () {
+    localStorage.setItem("tasks1", inputTask5.value);
+  });
+
+  saveButton2.addEventListener("click", function () {
+    localStorage.setItem("tasks2", inputTask6.value);
+  });
+
+  saveButton3.addEventListener("click", function () {
+    localStorage.setItem("tasks3", inputTask7.value);
+  });
+
+  saveButton4.addEventListener("click", function () {
+    localStorage.setItem("tasks4", inputTask8.value);
+  });
+
+  saveButton5.addEventListener("click", function () {
+    localStorage.setItem("tasks5", inputTask9.value);
+  });
+
 
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -42,31 +95,5 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 });
 
-// function init() {
-//   localStorage.getItem("tasks");
-// }
 
-// function storeTasks() {
-//   // Stringify and set key in localStorage to todos array
-//   localStorage.setItem("tasks", JSON.stringify(tasks));
-// }
-
-// function changeColors() {
-
-//   if
-
-// }
-
-
-
-
-
-
-
-
-// saveButton.addEventListener("click", function () {
-
-//   localStorage.setItem("tasks", hour9);
-
-// });
 
